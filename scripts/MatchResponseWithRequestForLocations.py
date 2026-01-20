7441b09a082e52045eaaed18fc77945e90a61359
kurtmaekelberghe <kurt.maekelberghe@gmail.com>
2026-01-20 21:28:45 +0100

    Maakt een dataframe met de locaties voor de berekende route

---
+++ scripts/MatchResponseWithRequestForLocations.py
@@ -0,0 +1,17 @@
+def MatchResponseWithRequestForLocations(requestfile,responsefile):
+    # geeft terug : een dataframe met de locaties in volgorde van de response.
+    with open(requestfile, "r") as f:
+        RouteRequest = json.load(f)
+    tasks = RouteRequest["tasks"]  # data["tasks"] is een lijst van dictionaries
+    df_Request = pd.json_normalize(tasks)  # json_normalize maakt geneste structuren plat
+    AddIDentifier(df_Request)
+
+
+    df_response = pd.read_csv(responsefile, header=None, names=["tasks"])
+
+
+    df_response["tasks"] = df_response["tasks"].astype(str)
+    df_Request["id"] = df_Request["id"].astype(str)
+    df_result = df_response.merge(df_Request,left_on="tasks",right_on="id", how="left")
+
+    return df_result
